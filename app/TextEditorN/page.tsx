32a593d4d00d0da2c5eeff244df4be5c0d60590f
mant0u0 <mant0u.goo@gmail.com>
2025-02-09 00:59:44 +0800

    [Mantou] TS 修正 1

--- app/TextEditorN/page.tsx
+++ app/TextEditorN/page.tsx
@@ -1,6 +1,6 @@
 // page.tsx
 "use client";
-import React, { useState, useCallback, ChangeEvent, useEffect } from "react";
+import React, { useState, useCallback, ChangeEvent } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import {
