8f68ee2fbb51c5671f642b001243a19629c1b69d
mant0u0 <mant0u.goo@gmail.com>
2025-02-22 14:27:28 +0800

    [Mantou] 移除沒用到的載入

--- app/TextEditor/page.tsx
+++ app/TextEditor/page.tsx
@@ -1,7 +1,6 @@
 // page.tsx
 "use client";
 import React, { useState, useCallback, ChangeEvent } from "react";
-import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
 import {
